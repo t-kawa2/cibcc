96778f721ea9251f0d15e992d9fba5bd6f0aed44
t-kawa2 <t-kawa2@amber.plala.or.jp>
2026-03-16 15:51:26 +0900

    cibcc

---
+++ cibcc/Go/step12/sect1/test.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+assert() {
+	expected="$1"
+	input="$2"
+
+	./cibcc "$input" > tmp.s
+	gcc -static -o tmp tmp.s
+	./tmp
+	actual="$?"
+
+	if [ "$actual" = "$expected" ]; then
+		echo "$input => $actual"
+	else
+		echo "$input => $expected expected, but got $actual"
+		exit 1
+	fi
+}
+
+assert 0 'return 0;'
+assert 42 'return 42;'
+assert 21 'return 5+20-4;'
+assert 41 'return   12  +  34  -  5  ;'
+assert 8 'return 2+2*3;'
+assert 6 'return 16/2-2;'
+assert 15 'return 5*(9-6);'
+assert 4 'return (3+5)/2;'
+assert 10 'return - -10;'
+assert 10 'return - - +10;'
+assert 1 'return 2 > 1;'
+assert 0 'return 1 > 2;'
+assert 0 'return 2 < 1;'
+assert 1 'return 1 < 2;'
+assert 0 'return 1 == 2;'
+assert 1 'return 2 == 2;'
+assert 1 'return 1 != 2;'
+assert 0 'return 2 != 2;'
+assert 1 'return 1 <= 2;'
+assert 0 'return 2 <= 1;'
+assert 0 'return 1 >= 2;'
+assert 1 'return 2 >= 1;'
+assert 1 'return 1; 2; 3;'
+assert 2 '1; return 2; 3;'
+assert 3 'a=3; return a;'
+assert 8 'a=3; z=5; return a+z;'
+assert 3 'foo=3;return foo;'
+assert 8 'foo=3;bar=5;return foo+bar;'
+assert 3 'if(0) return 2; return 3;'
+assert 3 'if(1-1) return 2; else return 3;'
+assert 2 'if(1) return 2; return 3;'
+assert 2 'if(2-1) return 2; else return 3;'
+
+echo OK
+
---
+++ cibcc/Go/step12/sect3/parser.go
@@ -0,0 +1,316 @@
+package main
+
+import (
+	"fmt"
+	"reflect"
+)
+
+type NodeKind int
+
+const (
+	ND_ADD	NodeKind = iota
+	ND_SUB
+	ND_MUL
+	ND_DIV
+	ND_EQ
+	ND_NE
+	ND_LT
+	ND_LE
+	ND_ASSIGN
+	ND_RETURN
+	ND_IF
+	ND_WHILE
+	ND_FOR
+	ND_EXPR_STMT
+	ND_VAR
+	ND_NUM
+)
+
+type Node struct {
+	kind NodeKind
+	next *Node
+	lhs *Node
+	rhs *Node
+	cond *Node
+	then *Node
+	els *Node
+	init *Node
+	inc *Node
+	va *Var
+	val int
+}
+
+type Var struct {
+	next *Var
+	name []rune
+	offset int
+}
+
+type Program struct {
+	node *Node
+	locals *Var
+	stack_size int
+}
+
+var node *Node
+var locals *Var
+
+func new_Node(kind NodeKind, lhs *Node, rhs *Node) *Node {
+	return &Node{kind: kind, lhs: lhs, rhs: rhs}
+}
+
+func new_unary(kind NodeKind, expr *Node) *Node {
+	return &Node{kind: kind, lhs: expr}
+}
+
+func new_number(val int) *Node {
+	return &Node{kind: ND_NUM, val: val}
+}
+
+func peek(s []rune) bool {
+	if token.kind != TK_RESERVED {
+		return false
+	}
+	if len(s) != token.len {
+		return false
+	}
+	if !reflect.DeepEqual(token.str[:token.len], s) {
+		return false
+	}
+	return true
+}
+
+func consume(op []rune) *Token {
+	if !peek(op) {
+		return nil
+	}
+	tok := token
+	token = token.next
+	return tok
+}
+
+func consume_ident() *Token {
+	if token.kind != TK_IDENT {
+		return nil
+	}
+	tok := token
+	token = token.next
+	return tok
+}
+func expect_ident() []rune {
+	if token.kind != TK_IDENT {
+		return nil
+	}
+	s := token.str[:token.len]
+	token = token.next
+	return s
+}
+
+func expect(op []rune) {
+	if !peek(op) {
+		fmt.Println("not expected code")
+	}
+	token = token.next
+}
+
+func expect_number() int {
+	v := token.val
+	token = token.next
+	return v
+}
+
+func at_eof() bool {
+	return token.kind == TK_EOF
+}
+
+
+func new_var(va *Var) *Node {
+	return &Node{kind: ND_VAR, va: va}
+}
+
+func find_var(toks []rune) *Var {
+	for va := locals; va != nil; va = va.next {
+		if len(va.name) == len(toks) && reflect.DeepEqual(toks, va.name) {
+			return va
+		}
+	}
+	return nil
+}
+
+func push_var(name []rune) *Var {
+	va := &Var{name: name}
+	va.next = locals
+	locals = va
+	return va
+}
+
+func read_expr_stmt() *Node {
+	return new_unary(ND_EXPR_STMT, expr())
+}
+
+
+
+
+
+
+func program() *Program {
+	locals = nil
+
+	var head Node
+	head.next = nil
+	cur := &head
+
+	for !at_eof() {
+		cur.next = stmt()
+		cur = cur.next
+	}
+	return &Program{node: head.next, locals: locals}
+}
+
+func stmt() *Node {
+	if consume([]rune("return")) != nil {
+		node := new_unary(ND_RETURN, expr())
+		expect([]rune(";"))
+		return node
+	}
+	if consume([]rune("if")) != nil {
+		node := &Node{kind: ND_IF}
+		expect([]rune("("))
+		node.cond = expr()
+		expect([]rune(")"))
+		node.then = stmt()
+		if consume([]rune("else")) != nil {
+			node.els = stmt()
+		}
+		return node
+	}
+	if consume([]rune("while")) != nil {
+		node := &Node{kind: ND_WHILE}
+		expect([]rune("("))
+		node.cond = expr()
+		expect([]rune(")"))
+		node.then = stmt()
+		return node
+	}
+	if consume([]rune("for")) != nil {
+		node := &Node{kind: ND_FOR}
+		expect([]rune("("))
+		if consume([]rune(";")) == nil {
+			node.init = read_expr_stmt()
+			expect([]rune(";"))
+		}
+		if consume([]rune(";")) == nil {
+			node.cond = expr()
+			expect([]rune(";"))
+		}
+		if consume([]rune(")")) == nil {
+			node.inc = read_expr_stmt()
+			expect([]rune(")"))
+		}
+		node.then = stmt()
+		return node
+	}
+	node := new_unary(ND_EXPR_STMT, expr())
+	expect([]rune(";"))
+	return node
+}
+
+func expr() *Node {
+	return assign()
+}
+
+func assign() *Node {
+	node := equality()
+
+	if consume([]rune("=")) != nil {
+		node = new_Node(ND_ASSIGN, node, assign())
+	}
+
+	return node
+}
+
+func equality() *Node {
+	node := relational()
+	for {
+		if consume([]rune("==")) != nil {
+			node = new_Node(ND_EQ, node, relational())
+		} else if consume([]rune("!=")) != nil {
+			node = new_Node(ND_NE, node, relational())
+		} else {
+			return node
+		}
+	}
+}
+
+func relational() *Node {
+	node := add()
+	for {
+		if consume([]rune("<")) != nil {
+			node = new_Node(ND_LT, node, add())
+		} else if consume([]rune("<=")) != nil {
+			node = new_Node(ND_LE, node, add())
+		} else if consume([]rune(">")) != nil {
+			node = new_Node(ND_LT, add(), node)
+		} else if consume([]rune(">=")) != nil {
+			node = new_Node(ND_LE, add(), node)
+		} else {
+			return node
+		}
+	}
+}
+
+func add() *Node {
+	node := mul()
+	for {
+		if consume([]rune("+")) != nil {
+			node = new_Node(ND_ADD, node, mul())
+		} else if consume([]rune("-")) != nil {
+			node = new_Node(ND_SUB, node, mul())
+		} else {
+			return node
+		}
+	}
+}
+
+func mul() *Node {
+	node := unary()
+	for {
+		if consume([]rune("*")) != nil {
+			node = new_Node(ND_MUL, node, unary())
+		} else if consume([]rune("/")) != nil {
+			node = new_Node(ND_DIV, node, unary())
+		} else {
+			return node
+		}
+	}
+}
+
+func unary() *Node {
+	if consume([]rune("+")) != nil {
+		return unary()
+	}
+	if consume([]rune("-")) != nil {
+		return new_Node(ND_SUB, new_number(0), unary())
+	}
+	return primary()
+}
+
+func primary() *Node {
+	if consume([]rune("(")) != nil {
+		node := expr()
+		expect([]rune(")"))
+		return node
+	}
+
+	toks := expect_ident()
+	if toks != nil {
+		va := find_var(toks)
+		if va == nil {
+			va = push_var(toks)
+		}
+		return new_var(va)
+	}
+
+	return new_number(expect_number())
+}
+
